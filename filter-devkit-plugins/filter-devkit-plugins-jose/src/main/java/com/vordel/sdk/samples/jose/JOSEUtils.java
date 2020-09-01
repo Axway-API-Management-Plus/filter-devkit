a5a096a6d491332ed641ec28d47f353c56262cd2
Rodolphe de Saint Léger <rdesaintleger@axway.com>
2020-09-01 15:44:31 +0200

    Forgot to remove import

--- filter-devkit-plugins/filter-devkit-plugins-jose/src/main/java/com/vordel/sdk/samples/jose/JOSEUtils.java
+++ filter-devkit-plugins/filter-devkit-plugins-jose/src/main/java/com/vordel/sdk/samples/jose/JOSEUtils.java
@@ -18,7 +18,6 @@ import java.util.zip.DeflaterOutputStream;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.JOSEException;
