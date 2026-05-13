0d93e8ede95e703b9d588f837870300d57b8270d
Rodolphe de Saint Léger <rdesaintleger@axway.com>
2026-05-13 11:20:41 +0200

    Upgrade Quick Filter generation
    - Set bnd plugin version at one place
    - Removal of NLS unused message warnings
    - Make bundle generation more generic
    - Add the Jabber filter as tutorial (provided in old Gateways install)
    - Fix semi colon typo
    
    TODO
    
    - Jabber filter tutorial documentation
    - ScopesMatch exception in OAuth Filter

--- filter-devkit-studio/src/main/java/com/vordel/client/manager/filter/devkit/studio/script/advanced/AdvancedScriptResources.java
+++ filter-devkit-studio/src/main/java/com/vordel/client/manager/filter/devkit/studio/script/advanced/AdvancedScriptResources.java
@@ -7,11 +7,35 @@ public class AdvancedScriptResources extends NLS {
 
 	public static String ADVANCEDSCRIPT_PAGE_TITLE;
 	public static String ADVANCEDSCRIPT_PAGE_DESCRIPTION;
-
 	public static String ADVANCEDSCRIPT_PALETTE_NAME;
 
+	public static String ADVANCEDSCRIPT;
+	public static String ADVANCEDSCRIPT_LANGUAGE_NASHORN;
+	public static String ADVANCEDSCRIPT_LANGUAGE_RHINO;
+	public static String ADVANCEDSCRIPT_LANGUAGE_GROOVY;
+	public static String ADVANCEDSCRIPT_LANGUAGE_PYTHON;
+
+	public static String ADVANCEDSCRIPT_RESOURCES;
+
+	public static String ADVANCEDSCRIPT_ATTRIBUTES;
+	public static String ADVANCEDSCRIPT_ATTRIBUTES_REQUIRED;
+	public static String ADVANCEDSCRIPT_ATTRIBUTES_GENERATED;
+	public static String ADVANCEDSCRIPT_ATTRIBUTES_CONSUMED;
+
+	public static String SCRIPT_RESOURCE_NAME;
 	public static String ADD_SCRIPT_RESOURCE;
 	public static String EDIT_SCRIPT_RESOURCE;
+	public static String DELETE_SCRIPT_RESOURCE;
+
+	public static String SCRIPT_RESOURCE_NAME_LABEL;
+
+	public static String RESOURCE_REFERENCE_CHOICE;
+	public static String RESOURCE_REFERENCELABEL;
+	public static String RESOURCE_REFERENCETITLE;
+
+	public static String SELECTOR_RESOURCE_CHOICE;
+	public static String SELECTOR_EXPRESSION;
+	public static String SELECTOR_CLAZZ;
 
 	static {
 		// initialize resource bundle
